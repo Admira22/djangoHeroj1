570e588e117008492e798d95a82f5cdf2c49ff6a
Admira <admira.bakal@gmail.com>
2023-05-07 17:27:59 +0200

    database

--- heroj1/admin.py
+++ heroj1/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 
 
 from heroj1.models import NewCK
+from heroj1.models import Korisnik
+from heroj1.models import KorisnikProgres
+from heroj1.models import Lekcija
+from heroj1.models import Pitanje
+from heroj1.models import Odgovor
+
 # Register your models here.
 admin.site.register(NewCK)
+admin.site.register(Korisnik)
+admin.site.register(KorisnikProgres)
+admin.site.register(Lekcija)
+admin.site.register(Pitanje)
+admin.site.register(Odgovor)
